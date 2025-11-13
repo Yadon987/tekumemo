58e56225ceb89bb5b75760296423e5f5bc75afa0
Yadon987 <cvg.fhc.66@gmail.com>
2025-11-14 08:46:30 +0900

    Fix:テストユーザー認証の追加設定

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,10 +1,13 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test "should get index with authenticated user" do
     user = users(:one)
-    sign_in user
+    user.password = "password"
+    user.password_confirmation = "password"
+    user.save!
 
+    sign_in user
     get root_url
     assert_response :success
   end
