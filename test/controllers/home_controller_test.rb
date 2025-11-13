737f6e653e20e2ae9a74486d40d5709abb095ddf
Yadon987 <cvg.fhc.66@gmail.com>
2025-11-14 08:13:33 +0900

    Fix:テストの認証ができなかったためログインさせる設定に変更

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,6 +2,9 @@ require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
+    user = users(:one) 
+    sign_in user
+
     get root_url
     assert_response :success
   end
