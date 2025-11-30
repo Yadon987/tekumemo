abf2bffed8ac645f9280611286b27e44aa2aa592
Yadon987 <cvg.fhc.66@gmail.com>
2025-11-30 11:27:06 +0900

    style: RuboCopによる自動修正 (rails_helper.rb)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,7 +40,7 @@ require 'selenium-webdriver'
 
 RSpec.configure do |config|
   config.before(:each, type: :system, js: true) do
-    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400] do |options|
+    driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ] do |options|
       options.add_argument('no-sandbox')
       options.add_argument('disable-dev-shm-usage')
     end
